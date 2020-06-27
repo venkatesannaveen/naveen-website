3dea4c43a44acfd7f471089b07a71eb7f5c1812e
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-06-26 17:07:30 -0700

    Updated files

--- src/components/PubCard.js
+++ src/components/PubCard.js
@@ -4,24 +4,14 @@ class PubCard extends React.Component {
     render () {
         return (
             <a href={this.props.link} target='_blank' style={{'textDecoration': 'none', 'color': 'black'}}>
-                <div style={pubStyle} class='pub-card'>
-                    <em>{this.props.journal}</em> <br />
-                    <strong>{this.props.title}</strong> <br />
+                <div class='pub-card'>
+                    <span style={{'color': 'rgb(100, 100, 100)'}}><em>{this.props.journal}</em></span> <br />
+                    <span style={{'color': '#4838db', 'fontWeight': '800'}}>{this.props.title}</span> <br />
                     <div style={authorStyle}>{this.props.authors}</div>
                 </div>
             </a>
         );
     }
-}
-
-const pubStyle = {
-    'padding': '10px',
-    'marginLeft': '10%',
-    'marginTop': '20px',
-    'marginBottom': '20px',
-    'width': '80%',
-    'borderRadius': '5px',
-    'boxShadow': '0 0 2px rgba(0,0,0,0.12), 0 0 4px rgba(0,0,0,0.12), 0 0 8px rgba(0,0,0,0.12), 0 0 16px rgba(0,0,0,0.12), 0 0 32px rgba(0,0,0,0.12)'
 }
 
 const authorStyle = {
